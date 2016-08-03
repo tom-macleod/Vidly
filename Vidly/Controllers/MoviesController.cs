17de4cea48a0e10799227388d3a84aa2315dbf66
Tom <macleod2602@gmail.com>
2016-08-03 17:49:29 -0400

    Practicing creating controller actions

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { name = "Shrek" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        }
+    }
+}
